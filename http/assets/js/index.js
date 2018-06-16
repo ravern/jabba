5e71f9303091c36e1edc7771982b454be144be27
Ravern Koh <ravernkoh@gmail.com>
2018-06-16 21:12:33 +0800

    Change to reset other buttons when copy button clicked

--- http/assets/js/index.js
+++ http/assets/js/index.js
@@ -1,29 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
     clipboard();
 });
 
 // Configures the copy buttons.
 function clipboard() {
     const clipboard = new ClipboardJS('.copy');
-    clipboard.on('success', function(e) {
+
+    clipboard.on('success', function (e) {
+        // Reset the rest of the copy buttons.
+        $('.copy').each(function() {
+            $(this).text('COPY').prop('disabled', false).removeClass('copied');
+        })
+
         // Update the element and reset after a while.
         const elem = $(e.trigger);
-        const text = elem.text();
         elem.text('COPIED').prop('disabled', true).addClass('copied');
-        setTimeout(function() {
-            elem.text(text).prop('disabled', false).removeClass('copied');
+        setTimeout(function () {
+            elem.text('COPY').prop('disabled', false).removeClass('copied');
         }, 3000);
     })
 }
 
-// Checks if the remote is loaded, if not the local is loaded.
+// Checks if the remote stylesheet is loaded, if not, load the local copy.
 function load(remote, local) {
-    $.each(document.styleSheets, function(i, sheet){
+    $.each(document.styleSheets, function (i, sheet) {
         if (sheet.href == remote) {
-          var rules = sheet.rules ? sheet.rules : sheet.cssRules;
-          if (rules.length == 0) {
-            document.write(`<link rel="stylesheet" href="${local}" />`);
-          }
+            var rules = sheet.rules ? sheet.rules : sheet.cssRules;
+            if (rules.length == 0) {
+                document.write(`<link rel="stylesheet" href="${local}" />`);
+            }
         }
     });
 }
